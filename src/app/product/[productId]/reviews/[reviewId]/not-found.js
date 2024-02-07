5be7f708796ce35d45a20317ddcc89ceb213dcba
PasinduS96 <pasindubahagya@gmail.com>
2024-02-07 13:54:31 +0530

    not found page level

---
+++ src/app/product/[productId]/reviews/[reviewId]/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReviewNotFound = () => {
+    return (
+        <div>
+         <h1>Review Not Found</h1>  
+        </div>
+    );
+}
+
+export default ReviewNotFound;
--- src/app/product/[productId]/reviews/[reviewId]/page.js
+++ src/app/product/[productId]/reviews/[reviewId]/page.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { notFound } from 'next/navigation';
 
 const Reviews = ({params}) => {
+    if(parseInt(params.reviewId) > 1000) {
+        notFound();
+    }
     return (
         <div>
             <h1>Review {params.reviewId} of Product {params.productId}</h1>
